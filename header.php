81be91aaaf4d1743d2652e234968c15384ef1d04
Thomas van den Ouweland <thomas.ouweland@rocdeleijgraaf.nl>
2022-03-23 11:23:16 +0100

    Bootstrap aanpassingen

--- header.php
+++ header.php
@@ -2,9 +2,23 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <!-- MDB icon -->
+    <link rel="icon" href="img/mdb-favicon.ico" type="image/x-icon" />
+    <!-- Font Awesome -->
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+    />
+    <!-- Google Fonts Roboto -->
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap"
+    />
+    <!-- MDB -->
+    <link rel="stylesheet" href="css/mdb.min.css" />
 </head>
 <body>
 <?php 
@@ -14,16 +28,19 @@ session_start();
         <a href="index.php">
             <img style="max-width: 100px;" src="img/Lays-Logo.png" alt="logo">
         </a>
-            <ul>
-                <li><a href="index.php">Home</a></li>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item"><a class="nav-link"  href="index.php">Home</a></li>
+                <li class="nav-item"><a class="nav-link"  href="style.php">TEST</a></li>
 
                 <?php 
                 if (isset($_SESSION['userId'])){
-                    echo '<li><a href="profile.php">Mijn profiel</a></li>';
-                    echo '<li><a href="pasjes.php">Mijn pasjes</a></li>';
+                    //Wanneer je een userlevel hebt
+                    echo '<li class="nav-item"><a class="nav-link"  href="profile.php">Mijn profiel</a></li>';
+                    echo '<li class="nav-item"><a class="nav-link"  href="pasjes.php">Mijn pasjes</a></li>';
                 }else{
-                    echo '<li><a href="contact.php">Contact</a></li>';
-                    echo '<li><a href="login.php">Inloggen</a></li>';
+                    //wanneer je niet ingelogd bent
+                    echo '<li class="nav-item"><a class="nav-link"  href="contact.php">Contact</a></li>';
+                    echo '<li class="nav-item"><a class="nav-link"  href="login.php">Inloggen</a></li>';
                 }
                 ?>
             </ul>
---
+++ style.php
@@ -0,0 +1,13 @@
+<?php 
+require "header.php";
+?>
+
+    <section>
+    <div>
+        <h1>Dit is de test pagina</h1>
+    </div>
+    </section>
+
+<?php
+require "footer.php";
+?>
