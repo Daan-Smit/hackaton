d8e743bdf9606fbbe7177e6ecdc71aae1a8125b3
Thomas van den Ouweland <thomas.ouweland@rocdeleijgraaf.nl>
2022-03-24 21:36:42 +0100

    commit

--- profile.php
+++ profile.php
@@ -5,14 +5,11 @@ require "header.php";
     <h2>Mijn profiel</h2>
 </div>
 <div class="container main">
-    <h2>Wat heb ik bespaard:</h2>
-    <h3>€38</h3>
-    <br>
-    <h2>Wat heb ik opgehaald:</h2>
-    <h3>€43</h3>
-
     <form action="profiel_wachtwoord_wijzigen.php">
         <button type="submit" class="btn">Wijzig uw wachtwoord</button>
+    </form>
+    <form action="index.php">
+        <button type="submit" class="btn">Uitloggen</button>
     </form>
 </div>
 
