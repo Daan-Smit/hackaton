6f0aab04cf4eb8bd62c1b3e7c1ae9def87ef43c6
Thomas van den Ouweland <thomas.ouweland@rocdeleijgraaf.nl>
2022-03-24 16:22:52 +0100

    comit

--- index.php
+++ index.php
@@ -2,12 +2,20 @@
 require "header.php";
 ?>
 <div class="header_img">
-    <h1>Stichting Leergeld</h1>
+    <h1>Welkom bij<br>Stichting Leergeld</h1>
+    <h2>Alle kinderen mogen meedoen!</h2>
 </div>
-<div>
-    <h1>Dit is de Home Pagina</h1>
+<div class="container main">
+    <p>In Nederland leeft 1 op de 12 kinderen in armoede. 
+        Deze kinderen kunnen om financiële redenen niet meedoen aan 
+        activiteiten die voor hun leeftijdsgenootjes heel normaal zijn. 
+        Zij staan vaak letterlijk aan de zijlijn. 
+        Leergeld wil deze kinderen mee laten doen!</p>
+    <form action="doneren.php">
+        <button type="submit" class="btn">Doneer nu zelf</button>
+    </form>
+    <p>de top 3 opbrengsten:</p>
 </div>
-
 <?php
 require "footer.php";
 ?>
