b91205f5b7dfb2f3ff3724ab9024016bcde23bfa
Daan-Smit <daan.smit@kpnmail.nl>
2022-03-24 12:58:29 +0100

    qrcode

--- qrcode.php
+++ qrcode.php
@@ -1,10 +1,12 @@
-
+<head>
+    <link rel="stylesheet" href="css/extracss.css" />
+</head>
+<button style="margin-left: 90%;"><a href="info_bedrijf.php">Vorige pagina</a></button>
 
 <?php
 $pasnummer = "34k4nk4678kl2";
-$code = '<center><img src="https://chart.googleapis.com/chart?chs=500x500&cht=qr&chl='. $pasnummer .'" title="QR Code"></center>';
+$code = '<center><img class="qrcode" src="https://chart.googleapis.com/chart?chs=500x500&cht=qr&chl='. $pasnummer .'" title="QR Code"></center>';
 echo $code;
-
-
+echo "<center>Dit is jou pascode: " . $pasnummer . "</center>"; 
 ?>
 
