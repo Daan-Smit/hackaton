63d1936167164abc5559125a539242d6f9c57cf7
Thomas van den Ouweland <thomas.ouweland@rocdeleijgraaf.nl>
2022-03-24 15:00:56 +0100

    commit

--- index.php
+++ index.php
@@ -4,11 +4,9 @@ require "header.php";
 <div class="header_img">
     <h1>Stichting Leergeld</h1>
 </div>
-    <section>
-    <div>
-        <h1>Dit is de Home Pagina</h1>
-    </div>
-    </section>
+<div>
+    <h1>Dit is de Home Pagina</h1>
+</div>
 
 <?php
 require "footer.php";
