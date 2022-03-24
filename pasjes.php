383ed48c58f5acaff962b7624eb2f34648f0b3e3
Thomas van den Ouweland <thomas.ouweland@rocdeleijgraaf.nl>
2022-03-24 10:41:52 +0100

    Commit

--- pasjes.php
+++ pasjes.php
@@ -9,15 +9,23 @@ require "header.php";
     $x = 10;
     $bedrijfsnaam = "Jumbo";
     $link = '#';
+    $kortingspercentage = "5%";
     ?>
     <div class="kaart_box">
         <?php 
         while($x >= 1){
-            echo "<a href='$link'><div class='card'>
-                    <div class='card-body Jumbo'>
-                        <h3>$bedrijfsnaam</h3>
+            echo "<div class='kaart card '>
+                    <div class='card-body kaart-body Jumbo'>
+                        <div class='row rij'>
+                            <div class='col-6'>
+                                <span>$bedrijfsnaam<span>
+                            </div>
+                            <div class='col-6'>
+                                <span class='procent'>$kortingspercentage</span>
+                            </div>
+                        </div>
                     </div>
-                  </div></a>";
+                  </div>";
             $x = $x - 1;
         }
         ?>
